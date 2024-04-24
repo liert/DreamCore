4e028c7f937526c1effe2edb4dbff8f9196e713c
liert <2102156704@qq.com>
2024-04-24 19:10:31 +0800

    优化

--- src/main/java/com/github/liert/Listens.java
+++ src/main/java/com/github/liert/Listens.java
@@ -2,18 +2,12 @@ package com.github.liert;
 
 import com.github.liert.Config.Settings;
 import com.github.liert.SQL.SQLQuery;
-import org.bukkit.Bukkit;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.event.server.RemoteServerCommandEvent;
-import org.bukkit.event.server.ServerCommandEvent;
-
 import org.bukkit.event.player.PlayerLoginEvent.Result;
+import org.bukkit.event.server.RemoteServerCommandEvent;
 
 public class Listens implements Listener {
 
@@ -28,27 +22,27 @@ public class Listens implements Listener {
 
     @EventHandler
     public void onRemoteCommand(RemoteServerCommandEvent e) {
-        CommandSender commandSender = e.getSender();
-        Bukkit.getConsoleSender().sendMessage(e.getCommand());
-        String[] args = e.getCommand().split(" ");
-        if (args.length < 1) {
-            return;
-        }
-        if (args[0].equals("wl") && args[1].equals("add")) {
-            if (args.length < 4) {
-                commandSender.sendMessage("Usage: /wl add <id>");
-                return;
-            }
-            String result = SQLQuery.bind(args[2], args[3]);
-            commandSender.sendMessage(result);
-        }
-        if (args[0].equals("wl") && args[1].equals("del")) {
-            if (args.length < 4) {
-                commandSender.sendMessage("Usage: /wl del <id>");
-                return;
-            }
-            String result = SQLQuery.delBind(args[2], args[3]);
-            commandSender.sendMessage(result);
-        }
+//        CommandSender commandSender = e.getSender();
+//        Bukkit.getConsoleSender().sendMessage(e.getCommand());
+//        String[] args = e.getCommand().split(" ");
+//        if (args.length < 1) {
+//            return;
+//        }
+//        if (args[0].equals("wl") && args[1].equals("add")) {
+//            if (args.length < 4) {
+//                commandSender.sendMessage("Usage: /wl add <id>");
+//                return;
+//            }
+//            String result = SQLQuery.bind(args[2], args[3]);
+//            commandSender.sendMessage(result);
+//        }
+//        if (args[0].equals("wl") && args[1].equals("del")) {
+//            if (args.length < 4) {
+//                commandSender.sendMessage("Usage: /wl del <id>");
+//                return;
+//            }
+//            String result = SQLQuery.delBind(args[2], args[3]);
+//            commandSender.sendMessage(result);
+//        }
     }
 }
